a40ad0cf1860f94e36b26bedee54c58c76b6b183
Nitin Sijwali <sijwalinitin@gmail.com>
2020-09-05 23:09:13 +0530

    fixed firebase issue

--- src/Account.js
+++ src/Account.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './Account.css';
 import { useStateValue } from './StateProvider'
 import { actionTypes } from './reducer'
